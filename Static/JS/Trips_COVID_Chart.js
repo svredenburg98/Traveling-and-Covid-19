c5db5dcb7b6433bab90ef4d8834a32c33bda3abe
Change vc-adams <vincent.c.adams@gmail.com>
2020-12-11 23:14:54 -0600

    Add the charts .js file and the a dig tag in the HTML.

--- Static/JS/Trips_COVID_Chart.js
+++ Static/JS/Trips_COVID_Chart.js
@@ -1,44 +1,30 @@
-d3.json("/data").then(function (data) {
+d3.csv("data").then(function (data) {
     // console.log(data)
 
-    var trips = []
-    var covid_cases = []
-    var covid_deaths = []
+    var trips = data.map(countyTrips => countyTrips.trips)
+    var covidCases = data.map(countyCovidCases => countyCovidCases.covid_cases)
+    var covidDeaths = data.map(countyDeaths => countyDeaths.covid_deaths)
+    var countyName = data.map(countyName => countyName.county_name_county)
 
-    data.forEach(function (data) {
-        // data.trips = +data.trips;
-        // console.log(data.trips)
-        trips.push(data.trips)
-    });
-    console.log(trips)
-
-    data.forEach(function (data) {
-        // data.trips = +data.trips;
-        // console.log(data.trips)
-        covid_cases.push(data.covid_cases)
-    });
-    console.log(covid_cases)
-
-    data.forEach(function (data) {
-        // data.trips = +data.trips;
-        // console.log(data.trips)
-        covid_deaths.push(data.covid_deaths)
-    });
-    console.log(covid_deaths)
 
     var trace1 = {
         x: trips,
-        y: covid_cases,
+        y: covidCases,
         mode: 'markers',
         type: 'scatter',
-        name: 'Team A',
-        text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-        marker: { size: 12 }
+        name: 'County',
+        text: countyName,
+        marker: {
+            size: 15,
+            color: "black"
+        },
 
     };
 
 
     var data = [trace1];
+
+
     var layout = {
         xaxis: {
             title: "Number of Trips Taken",
@@ -46,58 +32,44 @@ d3.json("/data").then(function (data) {
         },
         yaxis: {
             title: "COVID-19 Cases",
-            range: [0, d3.max(data, d => d.covid_cases)]
+            range: [0, d3.max(data, d => d.covid_cases)],
+            autorange: true
         },
-        title: 'Trips vs COVID-19 Cases'
+        title: 'Trips vs COVID-19 Cases',
+        plot_bgcolor: "white"
     }
     Plotly.newPlot('bar', data, layout);
+
+
+
+
+    var trace2 = {
+        x: trips,
+        y: covidDeaths,
+        mode: 'markers',
+        type: 'scatter',
+        name: 'County',
+        text: countyName,
+        marker: {
+            size: 15,
+            color: "red"
+        }
+    };
+
+    var data = [trace2];
+    var layout = {
+        xaxis: {
+            title: "Number of Trips Taken",
+            range: [0, d3.max(data, d => d.trips)]
+        },
+        yaxis: {
+            title: "COVID-19 Deaths",
+            range: [0, d3.max(data, d => d.covid_deaths)],
+            autorange: true
+        },
+        title: 'Trips vs COVID-19 Deaths',
+        plot_bgcolor: "white"
+    }
+    Plotly.newPlot('bar2', data, layout);
+
 })
-
-
-
-
-
-// var trace1 = {
-//     x: [1, 2, 3, 4, 5],
-//     y: [1, 6, 3, 6, 1],
-//     mode: 'markers',
-//     type: 'scatter',
-//     name: 'Team A',
-//     text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-//     marker: { size: 12 }
-// };
-// var trace2 = {
-//     x: [1.5, 2.5, 3.5, 4.5, 5.5],
-//     y: [4, 1, 7, 1, 4],
-//     mode: 'markers',
-//     type: 'scatter',
-//     name: 'Team B',
-//     text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
-//     marker: { size: 12 }
-// };
-// var data = [trace1, trace2];
-// var layout = {
-//     xaxis: {
-//         range: [0.75, 5.25]
-//     },
-//     yaxis: {
-//         range: [0, 8]
-//     },
-//     title: 'Data Labels Hover'
-// };
-// Plotly.newPlot('myDiv', data, layout);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
